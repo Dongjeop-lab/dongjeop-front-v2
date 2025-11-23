d6ba6681aedf84b4f01d647783e757004ab6ae64
Hyerim Yang <lovelimi1113@gmail.com>
2025-11-24 01:53:08 +0900

    feat: react-router 설치 및 RouterProvider 구조 세팅
    - React Router v7에서는 react-router-dom 대신 react-router 패키지 사용
    - createBrowserRouter와 RouterProvider로 기본 라우터 구조 구성
    - TODO: 라우트 설정 분리 (담당: 유정님)

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,21 @@ import './index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
 
 import App from './App.tsx';
 
+// TODO: 라우팅 설정 분리 및 보충 (유정님)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
