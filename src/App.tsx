924507f57bbe81439b37f58594218966393157cb
Pridesd <whwotjr98@naver.com>
2025-11-24 23:44:05 +0900

    feat: Add and integrate a modal component with state management.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import { css } from 'styled-system/css';
 
+import Modal from './components/modal';
 import { QueryProvider } from './lib/query-provider';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <QueryProvider>
-      <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>
+      <div
+        className={css({ fontSize: '2xl', fontWeight: 'bold' })}
+        onClick={() => setIsOpen(true)}
+      >
         Hello 🐼!
       </div>
+      <Modal
+        open={isOpen}
+        title='Modal'
+        onOpenChange={setIsOpen}
+      >
+        <p>Modal content</p>
+      </Modal>
     </QueryProvider>
   );
 };
