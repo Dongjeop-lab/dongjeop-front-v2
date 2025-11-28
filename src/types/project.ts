2b80f09395a66fc75a69573ef7d0772920cfe395
Pridesd <whwotjr98@naver.com>
2025-11-28 21:44:14 +0900

    feat: Add Project type definition and dummy project data to the dashboard page.

---
+++ src/types/project.ts
@@ -0,0 +1,14 @@
+export interface Project {
+  project_id: string;
+  name: string;
+  reviewer: string;
+  csv_file_name: string;
+  created_at: string; // TODO: string인지 Date인지 확인 필요
+  status: 'ANALYZING' | 'REVIEWING' | 'COMPLETED';
+  progress_info?: {
+    ai_analyzing_progress: number;
+    ai_analyzing_duration: number;
+    eviewing_store_total_count: number;
+    reviewing_store_completed_count: number;
+  };
+}
