9a33b76c22572bc0c404d8ea01133f672c00ba66
Hyerim Yang <lovelimi1113@gmail.com>
2026-01-12 00:24:26 +0900

    fix: 이미지 태그에 borderRadius 적용
    - 장소 상세 이미지 태그에 borderRadius 추가
    - 장소 목록 썸네일 이미지에 borderRadius 4px 추가 및 조건부 배경색 적용

--- src/pages/project/_components/store-list-table.tsx
+++ src/pages/project/_components/store-list-table.tsx
@@ -161,7 +161,7 @@ const StoreListTable = ({
                   position: 'relative',
                   height: '100%',
                   width: 'fit-content',
-                  backgroundColor: '#F3F4F6',
+                  backgroundColor: store.thumbnail_url ? '' : '#F3F4F6',
                   borderRadius: '4px',
                   overflow: 'hidden',
                 })}
@@ -171,6 +171,7 @@ const StoreListTable = ({
                   className={css({
                     height: '100%',
                     objectFit: 'contain',
+                    borderRadius: '4px',
                   })}
                   alt={`${store.name} 썸네일 이미지`}
                 />
